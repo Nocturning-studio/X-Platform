701fc6958eed0e23f2bdda99505d81573fe9666b
NSDeathman <forest.kir.325@gmail.com>
2026-02-17 20:55:35 +0300

    Удален ассемблер из xrCPU_Pipe

--- Sources/xrEngine/xrCPU_Pipe/xrCPU_Pipe.cpp
+++ Sources/xrEngine/xrCPU_Pipe/xrCPU_Pipe.cpp
@@ -5,46 +5,39 @@
 #pragma hdrstop
 
 extern xrSkin1W xrSkin1W_x86;
-extern xrSkin1W xrSkin1W_3DNow;
-// extern xrSkin1W		xrSkin1W_SSE;
 extern xrSkin2W xrSkin2W_x86;
 extern xrSkin2W xrSkin2W_SSE;
-extern xrSkin2W xrSkin2W_3DNow;
-// extern xrBoneLerp		xrBoneLerp_x86;
-// extern xrBoneLerp		xrBoneLerp_3DNow;
 extern xrM44_Mul xrM44_Mul_x86;
-extern xrM44_Mul xrM44_Mul_3DNow;
-extern xrM44_Mul xrM44_Mul_SSE;
 extern xrTransfer xrTransfer_x86;
-extern xrMemCopy_8b xrMemCopy_MMXSSE3DNow;
 extern xrMemCopy_8b xrMemCopy_x86;
 extern xrMemFill_32b xrMemFill32_MMX;
 
+// Копирование памяти (замена удалённой xrMemCopy_x86)
+void __stdcall xrMemCopy_x86(LPVOID dest, const void* src, u32 count)
+{
+	// Используем стандартную функцию memcpy
+	std::memcpy(dest, src, count);
+}
+
+// Заполнение памяти 32-битными значениями (замена удалённой xrMemFill32_MMX)
+void __stdcall xrMemFill32_MMX(LPVOID ptr, u32 count, u32 value)
+{
+	// ptr указывает на область памяти, count – количество dword (4-байтных слов)
+	u32* p = static_cast<u32*>(ptr);
+	for (u32 i = 0; i < count; ++i)
+		p[i] = value;
+
+	// Альтернативный вариант с использованием std::fill_n:
+	// std::fill_n(static_cast<u32*>(ptr), count, value);
+}
+
 void __cdecl xrBind_PSGP(xrDispatchTable* T, DWORD dwFeatures)
 {
-	// generic
 	T->skin1W = xrSkin1W_x86;
 	T->skin2W = xrSkin2W_x86;
-	// T->blerp	= xrBoneLerp_x86;
 	T->m44_mul = xrM44_Mul_x86;
 	T->transfer = xrTransfer_x86;
 	T->memCopy = xrMemCopy_x86;
 	T->memFill = NULL;
 	T->memFill32 = xrMemFill32_MMX;
-
-	// SSE
-	if (CPU::ID.hasFeature(CpuFeature::Sse))
-	{
-		T->memCopy = xrMemCopy_MMXSSE3DNow;
-		//T->skin2W	= xrSkin2W_SSE;
-	}
-
-	// 3dnow!
-	if (CPU::ID.hasFeature(CpuFeature::_3dNow))
-	{
-		// T->skin1W	= xrSkin1W_3DNow;
-		//  T->blerp	= xrBoneLerp_3DNow;
-		T->memCopy = xrMemCopy_MMXSSE3DNow;
-		// T->skin2W	= xrSkin2W_3DNow;
-	}
 }
