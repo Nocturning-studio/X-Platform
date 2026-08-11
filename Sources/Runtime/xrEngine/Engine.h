3fb67d04ca2254fb5883b16a8485661614d5d89a
NSDeathman <nsdeathmanofficial@gmail.com>
2026-08-11 04:12:02 +0400

    Project filesystem reorganization

---
+++ Sources/Runtime/xrEngine/Engine.h
@@ -0,0 +1,161 @@
+////////////////////////////////////////////////////////////////////////////////
+// Created: 14.01.2025
+// Author: NSDeathman
+// Refactored code: X-Ray class realization
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+////////////////////////////////////////////////////////////////////////////////
+class ENGINE_API CGameFont;
+struct xrDispatchTable;
+////////////////////////////////////////////////////////////////////////////////
+#include "stdafx.h"
+#include "EventAPI.h"
+#include "xrBind_PSGP.h"
+#include "LevelManager.h"
+#include "FontManager.h"
+#include "debug_ui.h"
+#include "pure.h"
+#include "WindowManager.h"
+#include "TimeManager.h"
+#include "ThreadManager.h"
+#include "RenderView.h"
+#include "ResourceManager.h"
+////////////////////////////////////////////////////////////////////////////////
+class ENGINE_API CSheduler;
+class ENGINE_API CLevelLoadingScreen;
+class ENGINE_API CGameStateManager;
+class ENGINE_API CStats;
+class ENGINE_API CResourceManager;
+////////////////////////////////////////////////////////////////////////////////
+// Class creation/destroying interface
+extern "C"
+{
+	typedef DLL_API DLL_Pure* __cdecl Factory_Create(CLASS_ID CLS_ID);
+	typedef DLL_API void __cdecl Factory_Destroy(DLL_Pure* O);
+};
+
+// Tuning interface
+extern "C"
+{
+	typedef void __cdecl VTPause(void);
+	typedef void __cdecl VTResume(void);
+};
+
+struct ENGINE_API CEngineEvents
+{
+	CRegistrator<pureFrame> Frame;
+	CRegistrator<pureRender> Render;
+	CRegistrator<pureAppActivate> AppActivate;
+	CRegistrator<pureAppDeactivate> AppDeactivate;
+	CRegistrator<pureAppStart> AppStart;
+	CRegistrator<pureAppEnd> AppEnd;
+	CRegistrator<pureDeviceReset> DeviceReset;
+};
+
+class ENGINE_API CEngine
+{
+  public:
+	typedef fastdelegate::FastDelegate0<bool> LOADING_EVENT;
+
+  private:
+	HMODULE hGame;
+	HMODULE hRender;
+	HMODULE hTuner;
+	HMODULE hOptick;
+
+	bool m_bLoaded;
+
+	xr_list<LOADING_EVENT> m_loading_events;
+
+  public:
+	Factory_Create* pCreate;
+	Factory_Destroy* pDestroy;
+
+	BOOL tune_enabled;
+	VTPause* tune_pause;
+	VTResume* tune_resume;
+
+	// Используются очень часто - встраиваем 
+	// инклуды с их реализациями в Engine.h
+	CEventAPI Event;
+	CLevelManager LevelManager;
+	CFontManager FontManager;
+	CDebugUI DebugUI;
+	CWindowManager WindowManager;
+	CTimeManager TimeManager;
+	CThreadManager ThreadManager;
+	CEngineEvents Events;
+	CRenderView RenderView; 
+
+	// Используются редко, их инклудим отдельно
+	CGameStateManager* GameStateManager;
+	CLevelLoadingScreen* LoadingScreen;
+	CSheduler* Sheduler;
+	CStats* Statistic;
+	CResourceManager* ResourceManager;
+
+	public:
+	// Конструктор/Деструктор
+	CEngine();
+	~CEngine();
+
+	void Run(); // Запуск Loop (Init -> Loop -> Destroy)
+
+	bool IsLoaded()
+	{
+		return m_bLoaded;
+	}
+
+	void SetLoaded()
+	{
+		m_bLoaded = TRUE;
+	}
+
+	void SetUnloaded()
+	{
+		m_bLoaded = FALSE;
+	}
+
+	xr_list<LOADING_EVENT> GetLoadingEvents()
+	{
+		return m_loading_events;
+	}
+
+	void AddLoadingEvent(LOADING_EVENT _Event)
+	{
+		m_loading_events.push_back(_Event);
+	}
+
+	void AddLoadingEventFront(LOADING_EVENT _Event)
+	{
+		m_loading_events.push_front(_Event);
+	}
+
+	void PopLoadingEvent()
+	{
+		m_loading_events.pop_front();
+	}
+
+	private:
+	// Основные жизненные циклы (Методы, которые остались)
+	bool Initialize(); // Вся инициализация (Console, Sound, Device, Input, DLLs) теперь здесь
+	void ProcessEventLoop();
+	void ProcessFrame();
+	bool CheckLoadingEvents(); // Обработка событий загрузки (true если событие обработано)
+	void UpdateGameLogic(); // Логика мира (бывший OnFrame)
+	void Destroy(); // Очистка ресурсов
+};
+////////////////////////////////////////////////////////////////////////////////
+// Объявляем глобальный указатель, чтобы его видели другие .cpp файлы
+extern ENGINE_API CEngine* g_Engine;
+
+// "Обманываем" старый код, который пишет Engine.Event...
+#define Engine (*g_Engine)
+
+#define NEW_INSTANCE(a) Engine.pCreate(a)
+#define DEL_INSTANCE(a)                                                                                                \
+	{                                                                                                                  \
+		Engine.pDestroy(a);                                                                                            \
+		a = NULL;                                                                                                      \
+	}
+////////////////////////////////////////////////////////////////////////////////
---
+++ Sources/Runtime/xrEngine/GameStateManager.cpp
@@ -0,0 +1,118 @@
+#include "stdafx.h"
+#include "GameStateManager.h"
+#include "Optick_Capture.h"
+#include "igame_level.h"
+#include "igame_persistent.h"
+#include "xr_input.h"
+#include "xr_ioconsole.h"
+#include "Engine.h"
+#include "std_classes.h"
+#include "GameFont.h"
+#include "resource.h"
+#include "LightAnimLibrary.h"
+#include "ispatial.h"
+#include "Text_Console.h"
+#include <process.h>
+#include "LevelLoadingScreen.h"
+
+void CGameStateManager::Initialize()
+{
+	eQuit = Engine.Event.Handler_Attach("KERNEL:quit", this);
+	eStart = Engine.Event.Handler_Attach("KERNEL:start", this);
+	eStartLoad = Engine.Event.Handler_Attach("KERNEL:load", this);
+	eDisconnect = Engine.Event.Handler_Attach("KERNEL:disconnect", this);
+
+	Engine.Events.Frame.Add(this, REG_PRIORITY_HIGH + 1000);
+
+#ifndef MASTER_GOLD
+	OptickCapture.Initialize();
+#endif
+}
+
+void CGameStateManager::Destroy()
+{
+#ifndef MASTER_GOLD
+	OptickCapture.Destroy();
+#endif
+
+	Engine.Events.Frame.Remove(this);
+
+	Engine.Event.Handler_Detach(eDisconnect, this);
+	Engine.Event.Handler_Detach(eStartLoad, this);
+	Engine.Event.Handler_Detach(eStart, this);
+	Engine.Event.Handler_Detach(eQuit, this);
+}
+
+void CGameStateManager::OnEvent(EVENT E, u64 P1, u64 P2)
+{
+	//OPTICK_EVENT("CGameStateManager::OnEvent");
+
+	if (E == eQuit)
+	{
+		PostQuitMessage(0);
+	}
+	else if (E == eStart)
+	{
+		LPSTR op_server = LPSTR(P1);
+		LPSTR op_client = LPSTR(P2);
+
+		// ... (логика main_menu) ...
+		{
+			Console->Execute("main_menu off");
+			Console->Hide();
+
+			g_pGamePersistent->PreStart(op_server);
+			g_pGameLevel = (IGame_Level*)NEW_INSTANCE(CLSID_GAME_LEVEL);
+
+			// --- ДЕЛЕГИРУЕМ ЗАГРУЗКУ ---
+			Engine.LoadingScreen->Show();
+			// ----------------------------
+
+			Msg("\nStart level loading...");
+			g_pGamePersistent->Start(op_server);
+			g_pGameLevel->net_Start(op_server, op_client);
+
+			// --- ДЕЛЕГИРУЕМ ЗАВЕРШЕНИЕ ---
+			Engine.LoadingScreen->Hide();
+			// -----------------------------
+		}
+		xr_free(op_server);
+		xr_free(op_client);
+	}
+	else if (E == eDisconnect)
+	{
+		if (g_pGameLevel)
+		{
+			g_pGameLevel->net_Stop();
+			DEL_INSTANCE(g_pGameLevel);
+			if ((FALSE == Engine.Event.Peek("KERNEL:quit")) && (FALSE == Engine.Event.Peek("KERNEL:start")))
+			{
+				Console->Execute("main_menu off");
+				Console->Execute("main_menu on");
+			}
+		}
+		g_pGamePersistent->Disconnect();
+	}
+}
+
+void CGameStateManager::OnFrame()
+{
+	// Обработка событий
+	Engine.Event.OnFrame();
+
+	// Обновление пространственных баз
+	g_SpatialSpace->update();
+	g_SpatialSpacePhysic->update();
+
+	// Звуковые события уровня
+	if (g_pGameLevel)
+		g_pGameLevel->SoundEvent_Dispatch();
+
+	// Для выделенного сервера обновление консоли здесь
+	if (g_dedicated_server)
+		Console->OnFrame();
+
+#ifndef MASTER_GOLD
+	OptickCapture.OnFrame();
+#endif
+}
