b81d835792a592267ee6fa1712233b07abc75cbb
NSDeathman <nsdeathmanofficial@gmail.com>
2026-09-23 02:28:03 +0400

    Более чистое API

--- Sources/Runtime/xrRenderBackend/framework.h
+++ Sources/Runtime/xrRenderBackend/framework.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "lib_io.h"
-
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <algorithm>
@@ -9,10 +7,6 @@
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
 #include <windows.h>
-
-#include <d3d9.h>
-#include <DXSDK/d3dx9.h>
-#include <d3dcompiler.h>
 
 #include <xrMath/xrMath.h>
 #include <xrCore/xrCore.h>
